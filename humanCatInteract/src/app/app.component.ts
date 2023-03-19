38ac9ab36cddc7c2a3a93274007956daec5d1408
iehkaatee <ykteerlink28@gmail.com>
2023-03-19 14:37:07 +0100

    update packages add answer array add todo readmee

--- humanCatInteract/src/app/app.component.ts
+++ humanCatInteract/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IAnswer } from './shared/models/answer.model';
 import { CatConnectService } from './shared/service/cat-connect.service';
 
 @Component({
@@ -10,6 +11,8 @@ export class AppComponent {
 
   _randCatObj!: any;
   _catSays!: string;
+  _answers: IAnswer[] = [];
+  _selectedItemId?: number;
 
   constructor(private catConnect: CatConnectService) { }
 
@@ -21,10 +24,17 @@ export class AppComponent {
   getNewCat(inputEl: HTMLInputElement) {
     this._catSays = inputEl.value;
     inputEl.value = '';
+
     this.catConnect.getRandomCat().subscribe(cat => {
       this._randCatObj = cat;
     });
+
+    this._answers.push({id: this._answers.length + 1, text: this.catSays});
   }
+
+  selectAnswer(id: number) {
+      this._selectedItemId = id;
+    }
 
   get RandomCatObject() {
     return this._randCatObj;
@@ -34,4 +44,7 @@ export class AppComponent {
     return this._catSays;
   }
 
+  get answers(): IAnswer[] {
+    return this._answers;
+  }
 }

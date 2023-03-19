9636bfbbd3c464dd4c9b61b59e69191e81deec3b
iehkaatee <33927294+iehkaatee@users.noreply.github.com>
2023-03-19 16:51:10 +0100

    Feature/xtra (#2)
    * update packages
    add answer array
    add todo readmee
    
    * add some cat functions

--- humanCatInteract/src/app/app.component.ts
+++ humanCatInteract/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IAnswer } from './shared/models/answer.model';
 import { CatConnectService } from './shared/service/cat-connect.service';
 
 @Component({
@@ -10,6 +11,8 @@ export class AppComponent {
 
   _randCatObj!: any;
   _catSays!: string;
+  _answers: IAnswer[] = [];
+  _selectedItemId = -1;
 
   constructor(private catConnect: CatConnectService) { }
 
@@ -19,11 +22,46 @@ export class AppComponent {
   }
 
   getNewCat(inputEl: HTMLInputElement) {
-    this._catSays = inputEl.value;
+
+    if (inputEl.value != '') {
+      this._answers.push({ id: this._answers.length, text: inputEl.value });
+
+    }
+
+    this._catSays = this.determineCatText(inputEl.value);
     inputEl.value = '';
-    this.catConnect.getRandomCat().subscribe(cat => {
+
+    this.catConnect.getRandomCat().subscribe((cat) => {
       this._randCatObj = cat;
     });
+
+    
+  }
+
+  determineCatText(inputText: string): string {
+    if (inputText !== '') {
+      return inputText;
+    }
+
+    // console.log(this._selectedItemId, this.answers)
+    if (this.selectedItemId >= 0) {
+      const selectedItem = this.answers.find((a) => {
+        // console.log(a.id, ' vs ', this.selectedItemId);
+        return a.id === this.selectedItemId;
+      });
+    
+      // console.log(selectedItem);
+      if (selectedItem) {
+        return selectedItem.text;
+      }
+    }
+
+    return '';
+  }
+
+  selectChange(e: any) {
+    console.log(e.target.value);
+    this._selectedItemId = +e.target.value;
   }
 
   get RandomCatObject() {
@@ -34,4 +72,11 @@ export class AppComponent {
     return this._catSays;
   }
 
+  get answers(): IAnswer[] {
+    return this._answers;
+  }
+
+  get selectedItemId(): number{
+    return this._selectedItemId;
+  }
 }
